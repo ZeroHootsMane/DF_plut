231f3e0686e8d513b3701b44775ccc8ba8c3a627
ZeroHootsMane <c905378@mvrht.com>
2016-07-07 17:43:26 +0200

    Create token.h

---
+++ token.h
@@ -0,0 +1,28 @@
+#ifndef TOKEN_H_INCLUDED
+#define TOKEN_H_INCLUDED
+
+/*
+Oh yea!
+Come on!
+
+You get the limo out front
+Hottest styles, every shoe, every color
+
+Yea when your famous it can be kinda fun
+It's really you but no one ever discovers
+
+In some ways you're just like all your friends
+But on stage you're a star
+
+You get the best of both worlds
+Chillin' out, take it slow
+Then you rock out the show
+
+You get the best of both worlds
+Mix it all together and you know that
+It's the best of both worlds
+The best of both worlds
+
+*/
+
+#endif // TOKEN_H_INCLUDED
